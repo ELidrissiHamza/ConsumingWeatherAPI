baa8b75e7a6a5f58bdd2a5e456e718676aee827b
ELidrissiHamza <hamza08elidrissi@gmail.com>
2023-03-14 01:55:00 +0100

    mongo

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN npm install
 #Cette instruction copie des fichiers ou des dossiers du système hôte dans l'image en cours de construction.
 COPY . .
 #Cette instruction indique aux utilisateurs de l'image les ports qui sont ouverts pour la communication externe.
-EXPOSE 3500
+EXPOSE 5400
 # Cette instruction définit la commande qui sera exécutée lorsque le conteneur sera démarré à partir de l'image.
-CMD ["npm","start"]
+CMD ["node","index.js"]
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express=require('express')
 const https=require('https');
 const bodyparser=require('body-parser')
+const mysql = require('mysql');
+const { query } = require('express');
+//const mongoose=require('mongoose');
+var MongoClient = require("mongodb").MongoClient;
+const { assert } = require('console');
 
 const app=express()
+//connect to mongodb
+//'mongodb+srv://user1:user1@weathercluster.jyab8oq.mongodb.net/mydb?retryWrites=true&w=majority'
+const dburl="mongodb://mongo-container:27017/myproject";
+const client = new MongoClient(dburl);
+async function connect(c , w ){
+  try{
+    await MongoClient.connect(dburl)
+      console.log('Connected to MongoDB')
+      const db = client.db('mydb');
+      await db.collection('mydb').insertOne({
+        city:c,
+        weather:w
+      });
+      
+    
+
+    }catch(error){
+      console.error(error)
+  }
+}
+
+
+
+  
+  
 app.use(bodyparser.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
+   
 res.sendFile(__dirname+"/form.html");
 
 })
@@ -12,6 +43,7 @@ app.post('/',(req,res)=>{
 console.log("the request is received");
  console.log(req.body.cityname) ;
  const query=req.body.cityname;
+ cityq=query;
 const apikey='a6bccd7ab6834c99394b16ef34bdc41f'
 const url='https://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+apikey+'&unit=metric';
 https.get(url,(response=>{
@@ -22,16 +54,21 @@ const weatherData=JSON.parse(data);
 //console.log(weatherData);
 const temp=weatherData.main.temp;
 const description=weatherData.weather[0].description;
+tempq=temp;
 //console.log(description);
 res.write("<h1 style='font-size: 2.5rem; font-weight: bold; text-align: center; margin-bottom: 1.5rem;'>The temerature in "+query+" is "+temp+" degree Kelvin (°K)</h1>")
 res.write("<p style='font-size: 1.2rem; text-align: center;'>the weather discription is"+ description+"</p>")
 res.write("<div style='text-align: center;'>")
 res.write(" <a href='/' style='font-size: 1.1rem; color: #0077cc; text-decoration: none; border: 2px solid #0077cc; padding: 0.5rem 1rem; border-radius: 5px; background-color: #fff;'>Retour</a>")
 res.write("</div>")  
+connect(query,temp);
+
 })
 }))
 })
-app.listen(3500,()=>{
-    console.log("our server is running at port 3500");
-})
 
+
+
+  app.listen(5400,()=>{
+    console.log("our server is running at port 5400");
+})
